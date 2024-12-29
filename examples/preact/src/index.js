9058f0c43a791fe8f9462928db1ce31977875152
rutruth <estheruthpraise@gmail.com>
2024-12-29 17:58:46 +0000

    filter react

--- examples/preact/src/index.js
+++
@@ -1,8 +0,0 @@
-import { h, render } from "preact";
-import App from "./app/app";
-
-import "todomvc-app-css/index.css";
-import "todomvc-common/base.css";
-import "./styles.css";
-
-render(<App />, document.querySelector(".todoapp"));
